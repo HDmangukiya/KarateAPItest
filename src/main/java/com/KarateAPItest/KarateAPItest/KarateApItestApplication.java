31274f91b8d3d3bc2e4fbec1ea81740ff0bcab41
hardik.mangukiya.003@gmail.com <Mydream@123>
2023-09-18 11:20:48 +1200

    Setup Project.

---
+++ src/main/java/com/KarateAPItest/KarateAPItest/KarateApItestApplication.java
@@ -0,0 +1,13 @@
+package com.KarateAPItest.KarateAPItest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateApItestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarateApItestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KarateAPItest/KarateAPItest/KarateApItestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KarateAPItest.KarateAPItest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarateApItestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
